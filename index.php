5120f4630a3b1fe6551a5df370b58e9d295ab1f3
Alfred Tenda Lumataw <alfrdt87@gmail.com>
2022-10-24 12:05:43 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+  echo "Hello World!<br>";
+
+  $nama = "Alfred";
+  $usia = 20;
+
+  echo "Nama saya <strong>$nama</strong>, saya berusia $usia tahun.<br>";
+
+  $namaAdik = "fred";
+  $usiaAdik = "15";
+
+  echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $usiaAdik tahun.<br>";
+  
+
+  $selisihUsia = $usia - $usiaAdik;
+  echo "Selisih usia saya dan adik saya $selisihUsia adalah tahun."
+
+?>
