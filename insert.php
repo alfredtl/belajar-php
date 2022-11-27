116b144873dd11173615c87fa5f52208789f5ee1
Alfred Tenda Lumataw <alfrdt87@gmail.com>
2022-11-27 23:25:40 +0800

    add update and delete feature

--- insert.php
+++ insert.php
@@ -1,43 +1,37 @@
 <?php
+    if (isset($_POST['submit'])){
+        //var_dump($_POST);
+        $nim = $_POST['nim'];
+        $nama = $_POST['nama'];
+        $id_jurusan = $_POST['id_jurusan'];
+        $tpt_lahir = $_POST['tpt_lahir'];
+        $tgl_lahir = $_POST['tgl_lahir'];
+        $gender = $_POST['gender'];
+        $alamat = $_POST['alamat'];
 
-// tangkap data 
-if (isset($_POST["submit"])){
-    $nim = $_POST['nim'];
-    $nama = $_POST["nama"];
-    $id_jurusan = $_POST['id_jurusan'];
-    $tpt_lahir = $_POST['tpt_lahir'];
-    $tgl_lahir = $_POST['tgl_lahir'];
-    $gender = $_POST['gender'];
-    $alamat = $_POST['alamat'];
-    
+        // Buat koneksi dengan MySQL
+        $con = mysqli_connect("localhost","root","","fakultas");
 
-    // 1. Buat koneksi dengan MySQL
-    $con = mysqli_connect("localhost", "root", "", "fakultas");
+        // Check connection
+        if (mysqli_connect_errno()) {
+            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+            exit();
+        }else{
+            echo 'koneksi berhasil';
+        }
 
-    // 2. Cek koneksi dengan MySQL
-    if (mysqli_connect_errno()){
-        echo "koneksi gagal ". mysqli_connect_error();
-    }else{
-        echo "koneksi berhasil";
+        $sql = "insert into mahasiswa (id_jurusan, nim, nama, jenis_kelamin, tempat_lahir, tanggal_lahir, alamat)
+        values ($id_jurusan,'$nim', '$nama', '$gender', '$tpt_lahir', '$tgl_lahir', '$alamat')";
+
+        if (mysqli_query($con, $sql)) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($con);
+        }
+          
+        mysqli_close($con);
     }
-
-    // buat sql query untuk insert ke database
-    // Buat query insert dan jalankan
-    $sql = "insert into mahasiswa (id_jurusan, nim, nama, jenis_kelamin, tempat_lahir, tanggal_lahir, alamat)
-            values ($id_jurusan,'$nim','$nama', '$gender', '$tpt_lahir', '$tgl_lahir', '$alamat')";
-
-
-    // jalankan query
-    if(mysqli_query($con,$sql)){
-        echo "Data berhasil ditambah";
-    } else{
-        echo "Ada error". mysqli_error();
-    }
-    mysqli_close($con);
-}
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -45,10 +39,12 @@ if (isset($_POST["submit"])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tambah Data Mahasiswa</title>
+    <title>Insert</title>
 </head>
 <body>
-    <form action="insert.php" method="post">
+    <h1>Tambah Data</h1>
+
+    <form action="" method="post">
         NIM: <input type="text" name="nim"><br>
         Nama: <input type="text" name="nama"><br>
         ID Jurusan: <input type="number" name="id_jurusan"><br>
@@ -57,7 +53,6 @@ if (isset($_POST["submit"])){
         Tanggal Lahir (yyyy-mm-dd): <input type="text" name="tgl_lahir"><br>
         Alamat: <input type="text" name="alamat"><br>
         <button type="submit" name="submit">Tambah Data</button>
-
     </form>
 </body>
 </html>
